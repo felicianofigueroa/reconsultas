ebd4e95e1c0500c021b1f1caf05e1bf79d3fff0f
felicianofigueroa <82834788+felicianofigueroa@users.noreply.github.com>
2026-04-09 10:54:46 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    //header('Content-Type: text/html; charset=utf-8');
+    require_once 'libs/database.php';
+    require_once 'libs/controller.php';
+    require_once 'libs/view.php';
+    require_once 'libs/model.php';
+    require_once 'libs/app.php';
+    require_once 'config/config.php';
+    require_once 'libs/fpdf.php';
+
+    $app = new App();
+    
+?>
