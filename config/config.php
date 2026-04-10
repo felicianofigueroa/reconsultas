4886c92de1cccdedbe6801ba5e93befe0564a626
felicianofigueroa <feliciano-figueroa@hotmail.com>
2026-04-10 10:31:25 -0700

    a

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set("America/Hermosillo");
+define('URL', 'http://192.166.127.57:81/COPYCONS/');
+
+define('HOST', 'localhost');
+define('DB', 'marketpla');
+define('USER', 'root');
+define('PASSWORD', "root");
+define('CHARSET', 'utf8mb4');
+?>
+
